c7a48f7488b89af64bff8385459cef8652787cc7
Humayun63 <humayun126224@gmail.com>
2023-06-07 07:19:12 +0600

    initial auth provider created

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,12 +1,29 @@
 import React, { createContext, useState } from 'react';
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext(null)
+const auth = getAuth(app)
 
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser)
+            setLoading(false)
+        })
+        return () => {
+            return unsubscribe()
+        }
+    }, [])
+
     const authInfo = {
-        user
+        user,
+        loading
     }
+    
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
